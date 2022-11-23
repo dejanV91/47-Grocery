4b5cfa397a663c1d12e0704ca7afd2dffdd74efb
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-23 16:08:01 +0100

    initial commit

---
+++ src/App2.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { Alert } from "./Alert";
+import { List } from "./List";
+
+function App2() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "please enter value", "danger");
+      // empty name
+    } else if (isEditing) {
+      // editing
+    } else {
+      showAlert(true, "item added to the list", "success");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+
+  const clearItems = () => {
+    showAlert(true, "empty list", "danger");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "item removed", "danger");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && (
+            <Alert {...alert} removeAlert={showAlert} list={list} />
+          )}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g. eggs"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItem} />
+            <button className="clear-btn" onClick={() => clearItems()}>
+              clear items
+            </button>
+          </div>
+        )}
+      </section>
+    </>
+  );
+}
+export default App2;
---
+++ src/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+export const List = ({ items, removeItem }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button className="edit-btn">
+                <FaEdit />
+              </button>
+              <button className="delete-btn" onClick={() => removeItem(id)}>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
